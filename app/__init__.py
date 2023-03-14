12d9b98ebf6dcb9329f8e8f8599f1c6164beb842
matilde <matildet@oasishoteles.com>
2023-03-14 11:23:49 -0500

    habilitar dirrecion de consumo

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ app.config.from_object('configuration.DevelopmentConfig')
 
 db = SQLAlchemy(app, session_options={"autoflush": False})
 
-cors = CORS(app, resources={r'/*':{'origin':['http://192.168.2.57:80','http://localhost:8080']}})
+cors = CORS(app, resources={r'/*':{'origin':['https://oasis-tlc.com','http://localhost:8080','http://localhost:80']}})
 
 from app.otlc.controllers.contract import otlc
 
