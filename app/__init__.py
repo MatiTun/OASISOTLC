5f64d2ead75c87eddee21f2b29794202316acd4a
matilde <matildet@oasishoteles.com>
2023-07-18 11:58:30 -0500

    se agrega subdominio staging.oasis-tlc.com

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ app.config.from_object('configuration.DevelopmentConfig')
 
 db = SQLAlchemy(app, session_options={"autoflush": False})
 
-cors = CORS(app, resources={r'/*':{'origin':['*']}})
+cors = CORS(app, resources={r'/*':{'origin':['http://localhost','http://192.168.2.57','http://staging.oasis-tlc.com']}})
 
 from app.otlc.controllers.contract import otlc
 
