ccecb69ee8dcb743f3636c80cac807ddd3b26278
matilde <matildet@oasishoteles.com>
2023-07-18 16:02:19 -0500

    ajuste cors OTLC

--- app/__init__.py
+++ app/__init__.py
@@ -7,12 +7,9 @@ app.config.from_object('configuration.DevelopmentConfig')
 
 db = SQLAlchemy(app, session_options={"autoflush": False})
 
-cors = CORS(app, resources={r'/*':{'origin':['*']}},allow_headers= ['Accept','Content-Type','Authorization','Access-Control-Allow-Credentials'])
-
-# cors = CORS(app, resources={r'/*':{'origin':['http://localhost','http://192.168.2.57','https://staging.oasis-tlc.com']}},
-# methods=['GET','POST'],
-# allow_headers= ['Accept','Content-Type','Authorization','Access-Control-Allow-Credentials'],
-# supports_credentials=True)
+cors = CORS(app, resources={r'/*':{'origin':['*']}},
+allow_headers= ['Accept','Content-Type','Authorization','Access-Control-Allow-Credentials'],
+supports_credentials=True)
 
 from app.otlc.controllers.contract import otlc
 
